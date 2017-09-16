4a9ec7c4bc1344eb39d6ace605dbf31af6b3b38c
junifar <junifar@gmail.com>
2017-09-16 16:56:48 +0700

    Adding CSS & JS bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [
     './app.component.css'
-  ],
-  styles:[
-
   ]
 })
 export class AppComponent {
